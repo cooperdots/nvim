7039dab42a1c505dae3b359d08d6b8a8e68267eb
Cooper b. Anderson <andersc7@rose-hulman.edu>
2022-03-27 01:31:40 -0700

    Modify indent-blankline for char_blankline

--- lua/cooper/indent.lua
+++ lua/cooper/indent.lua
@@ -5,10 +5,22 @@ require("packer").use {
   config = function()
     require("indent_blankline").setup {
       char = "┆";
+      char_blankline = "│";
+      show_foldtext = false;
+      max_indent_increase = 2;
       show_current_context = true;
-      show_current_context_start = true;
+      show_current_context_start = false;
       show_trailing_blankline_indent = false;
-      filetype_exclude = { "norg", "man", "NvimTree" }
+      filetype_exclude = {
+        "terminal"; "toggleterm";
+        "norg";
+        "man";
+        "NvimTree";
+        "TelescopePrompt";
+        "help";
+        "packer";
+        "UltestSummary"; "UltestOutput";
+      }
     }
   end
 }
