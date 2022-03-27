456a3729b45e2e1dda58521eabb77eb43e391f50
Cooper b. Anderson <andersc7@rose-hulman.edu>
2022-03-27 01:05:45 -0700

    Add neorg parsers for treesitter

--- lua/cooper/treesitter.lua
+++ lua/cooper/treesitter.lua
@@ -2,6 +2,7 @@ require("packer").use {
   -- Package info {{{
   "nvim-treesitter/nvim-treesitter";
   run = ":TSUpdate";
+  before = "neorg";
   requires = {
     { "nvim-treesitter/playground"; cmd = {
       "TSHighlightCapturesUnderCursor";
@@ -19,6 +20,20 @@ require("packer").use {
         files = { "src/parser.c", "src/scanner.cc" };
         branch = "main"
       };
+    }
+    parser_configs.norg_meta = {
+      install_info = {
+        url = "https://github.com/nvim-neorg/tree-sitter-norg-meta",
+        files = { "src/parser.c" },
+        branch = "main"
+      },
+    }
+    parser_configs.norg_table = {
+      install_info = {
+        url = "https://github.com/nvim-neorg/tree-sitter-norg-table",
+        files = { "src/parser.c" },
+        branch = "main"
+      },
     }
 
     require("nvim-treesitter.configs").setup {
@@ -42,6 +57,8 @@ require("packer").use {
         "yaml";
         "json";
         "norg";
+        "norg_meta";
+        "norg_table";
       };
       highlight = { enable = true, use_languagetree = true };
       indent = { enable = false };
