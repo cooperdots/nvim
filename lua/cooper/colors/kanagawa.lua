d46a72426f90e5ef1d56181de993b231e68460c1
Cooper b. Anderson <andersc7@rose-hulman.edu>
2022-03-27 01:15:51 -0700

    Modify Kanagawa overrides and make reloadable

--- lua/cooper/colors/kanagawa.lua
+++ lua/cooper/colors/kanagawa.lua
@@ -1,22 +1,35 @@
+function Kanagawa()
+  local colors = require("kanagawa.colors").setup()
+  require("kanagawa").setup {
+    keywordStyle = "NONE",
+    variablebuiltinStyle = "NONE",
+    overrides = {
+      Cursor = { bg = colors.springGreen, fg = colors.bg },
+      CursorIM = { bg = colors.crystalBlue },
+      CursorRM = { bg = colors.autumnRed },
+      CursorVM = { bg = colors.autumnYellow },
+      CursorCM = { bg = colors.oniViolet },
+
+      MatchParen = { style="none" };
+      Folded = { link="TSParameter" };
+      IncSearch = { link="DiffText" };
+      Search = { link="Visual" };
+      CopilotSuggestion = { fg = colors.fg_comment, bg = colors.bg_visual };
+
+      UltestSummaryInfo = { link="TSFunction" };
+      UltestSummaryNamespace = { link="TSConstructor" };
+
+      QuickScopePrimary = { style="reverse" };
+      QuickScopeSecondary = { style="underline" };
+    }
+  }
+  vim.cmd [[colorscheme kanagawa]]
+end
+
 require("packer").use {
   -- Package info {{{
   "rebelot/kanagawa.nvim";
+  before = "lualine.nvim";
   -- vim:set fdm=marker fdl=0: }}}
-  config = function()
-    local colors = require("kanagawa.colors").setup()
-    require("kanagawa").setup {
-      overrides = {
-        Cursor = { bg = colors.springGreen, fg = colors.bg },
-        CursorIM = { bg = colors.crystalBlue },
-        CursorRM = { bg = colors.autumnRed },
-        CursorVM = { bg = colors.autumnYellow },
-        CursorCM = { bg = colors.oniViolet },
-
-        MatchParen = { style="none" };
-        Folded = { link="TSParameter" };
-        IndentBlanklineContextStart = { style="none" };
-        CopilotSuggestion = { fg = colors.fg_comment, bg = colors.bg_visual };
-      }
-    }
-  end
+  config = Kanagawa
 }
