52c1ebf357021a44026d14f46d52ca423e4104c8
Cooper b. Anderson <andersc7@rose-hulman.edu>
2022-03-27 01:07:05 -0700

    Stop bufferline from showing lsp diagnostics

--- lua/cooper/bufferline.lua
+++ lua/cooper/bufferline.lua
@@ -6,7 +6,6 @@ require("packer").use {
   config = function()
     local bufferline = require("bufferline")
 
-    local icons = {error = '', warning = '', info = '', hint = ""}
     local hls = {
       error = '#ec5f67';
       warning = '#ecbe7b';
@@ -20,39 +19,13 @@ require("packer").use {
         modified_icon = '●';
         show_close_icon = false;
         show_buffer_close_icons = false;
-        diagnostics = "nvim_lsp";
         always_show_bufferline = true;
-        separator_style = "slant";
-        diagnostics_indicator = function(_, level)
-          if level == "other" then return "" end
-          return ' ' .. (icons[level] or level)
-        end;
       };
       highlights = {
         buffer_selected = { gui = "bold" };
         error_selected = { gui = "bold" };
         warning_selected = { gui = "bold" };
         info_selected = { gui = "bold" };
-
-        -- buffer_visible = { gui = nil };
-        -- error_visible = { gui = nil };
-        -- warning_visible = { gui = nil };
-        -- info_visible = { gui = nil };
-
-        background = { gui = "italic" };
-        error = { gui = "italic" };
-        warning = { gui = "italic" };
-        info = { gui = "italic" };
-
-        error_diagnostic = { guifg = hls.error };
-        error_diagnostic_visible = { guifg = hls.error };
-        error_diagnostic_selected = { guifg = hls.error };
-        warning_diagnostic = { guifg = hls.warning };
-        warning_diagnostic_visible = { guifg = hls.warning };
-        warning_diagnostic_selected = { guifg = hls.warning };
-        info_diagnostic = { guifg = hls.info };
-        info_diagnostic_visible = { guifg = hls.info };
-        info_diagnostic_selected = { guifg = hls.info };
 
         pick_selected = { gui = "italic", guifg = hls.grey };
         pick_visible = { gui = nil, guifg = hls.warning };
