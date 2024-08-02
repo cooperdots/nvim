0353286500ab2cb0773a1935fcbff6c999527028
Cooper b. Anderson <cooper@cooperuser.dev>
2024-08-01 20:52:54 -0500

    Update kanagawa config for new neovim

--- lua/cooper/plugins/kanagawa.lua
+++ lua/cooper/plugins/kanagawa.lua
@@ -1,43 +1,26 @@
 return {
 	"rebelot/kanagawa.nvim",
 	priority = 1000, -- Make sure to load this before all the other start plugins.
-	config = function()
-		local colors = require("kanagawa.colors").setup()
-		require("kanagawa").setup({
-			keywordStyle = {},
-			-- overrides = function(colors)
-			-- 	return {
-			overrides = {
-				Cursor = { bg = colors.springGreen },
-				CursorIM = { bg = colors.crystalBlue },
-				CursorRM = { bg = colors.autumnRed },
-				CursorVM = { bg = colors.autumnYellow },
-				CursorCM = { bg = colors.oniViolet },
+	opts = {
+		keywordStyle = { italic = false },
+		overrides = function(colors)
+			return {
+				Cursor = { bg = colors.palette.springGreen },
+				CursorIM = { bg = colors.palette.crystalBlue },
+				CursorRM = { bg = colors.palette.autumnRed },
+				CursorVM = { bg = colors.palette.autumnYellow },
+				CursorCM = { bg = colors.palette.oniViolet },
 
 				Visual = { reverse = true },
-
 				MatchParen = { bold = false },
-
-				-- rustSigil = { link = "Constant" },
 
 				Hlargs = { link = "Constant" },
 
 				QuickScopePrimary = { reverse = true },
 				QuickScopeSecondary = { underline = true },
-			},
-			-- 	}
-			-- end,
-		})
-	end,
-	-- opts = {
-	-- 	compile = true,
-	-- 	keywordStyle = {},
-	-- 	overrides = function()
-	-- 		return {
-	-- 			-- Cursor = { bg = colors.palette.springGreen },
-	-- 		}
-	-- 	end,
-	-- },
+			}
+		end,
+	},
 	init = function()
 		vim.cmd.colorscheme("kanagawa")
 
