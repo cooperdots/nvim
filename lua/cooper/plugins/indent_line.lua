7dcc06465e43f18a138bf403384f7d166326370e
Cooper b. Anderson <cooper@cooperuser.dev>
2024-08-13 19:20:36 -0500

    Enable indent blank lines plugin

---
+++ lua/cooper/plugins/indent_line.lua
@@ -0,0 +1,19 @@
+return {
+	{ -- Add indentation guides even on blank lines
+		"lukas-reineke/indent-blankline.nvim",
+		-- Enable `lukas-reineke/indent-blankline.nvim`
+		-- See `:help ibl`
+		main = "ibl",
+		opts = {
+			indent = {
+				char = "│",
+				tab_char = "│",
+			},
+			scope = {
+				char = "┃",
+				show_start = false,
+				show_end = false,
+			},
+		},
+	},
+}
