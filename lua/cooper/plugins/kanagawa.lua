bbc0a4cf6c170fdba78cbf96fbb5b6f94f14d99d
Cooper b. Anderson <cooper@cooperuser.dev>
2024-08-17 20:00:59 -0500

    Use underlines for diagnostics

--- lua/cooper/plugins/kanagawa.lua
+++ lua/cooper/plugins/kanagawa.lua
@@ -1,6 +1,8 @@
 return {
 	"rebelot/kanagawa.nvim",
 	priority = 1000, -- Make sure to load this before all the other start plugins.
+	---@module "kanagawa"
+	---@type KanagawaConfig
 	opts = {
 		keywordStyle = { italic = false },
 		overrides = function(colors)
@@ -10,6 +12,11 @@ return {
 				CursorRM = { bg = colors.palette.autumnRed },
 				CursorVM = { bg = colors.palette.autumnYellow },
 				CursorCM = { bg = colors.palette.oniViolet },
+
+				DiagnosticUnderlineError = { undercurl = false, underline = true },
+				DiagnosticUnderlineHint = { undercurl = false, underline = true },
+				DiagnosticUnderlineInfo = { undercurl = false, underline = true },
+				DiagnosticUnderlineWarn = { undercurl = false, underline = true },
 
 				Visual = { reverse = true },
 				MatchParen = { bold = false },
