a4b74a13f16de38f218ee8f231069d60f24d1c73
Cooper b. Anderson <cooper@cooperuser.dev>
2024-08-18 19:52:35 -0500

    Use nerdfont icons for diagnostics

--- lua/cooper/plugins/lsp.lua
+++ lua/cooper/plugins/lsp.lua
@@ -154,6 +154,17 @@ return { -- LSP Configuration & Plugins
 		local capabilities = vim.lsp.protocol.make_client_capabilities()
 		capabilities = vim.tbl_deep_extend("force", capabilities, require("cmp_nvim_lsp").default_capabilities())
 
+		vim.diagnostic.config({
+			signs = {
+				text = {
+					[vim.diagnostic.severity.ERROR] = "",
+					[vim.diagnostic.severity.WARN] = "",
+					[vim.diagnostic.severity.INFO] = "",
+					[vim.diagnostic.severity.HINT] = "",
+				},
+			},
+		})
+
 		-- Enable the following language servers
 		--  Feel free to add/remove any LSPs that you want here. They will automatically be installed.
 		--
