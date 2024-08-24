290be4fb0997e0d3acfc18aceaa99daf3317c4f4
Cooper b. Anderson <cooper@cooperuser.dev>
2024-08-24 15:14:51 -0500

    Swap goto type definition and goto declaration keybinds

--- lua/cooper/plugins/lsp.lua
+++ lua/cooper/plugins/lsp.lua
@@ -84,7 +84,7 @@ return { -- LSP Configuration & Plugins
 				-- Jump to the type of the word under your cursor.
 				--  Useful when you're not sure what type a variable is and you want to see
 				--  the definition of its *type*, not where it was *defined*.
-				map("<leader>D", require("telescope.builtin").lsp_type_definitions, "Type [D]efinition")
+				map("gD", require("telescope.builtin").lsp_type_definitions, "[G]oto Type [D]efinition")
 
 				-- Fuzzy find all the symbols in your current document.
 				--  Symbols are things like variables, functions, types, etc.
@@ -104,7 +104,7 @@ return { -- LSP Configuration & Plugins
 
 				-- WARN: This is not Goto Definition, this is Goto Declaration.
 				--  For example, in C this would take you to the header.
-				map("gD", vim.lsp.buf.declaration, "[G]oto [D]eclaration")
+				map("<leader>D", vim.lsp.buf.declaration, "Goto [D]eclaration")
 
 				-- The following two autocommands are used to highlight references of the
 				-- word under your cursor when your cursor rests there for a little while.
