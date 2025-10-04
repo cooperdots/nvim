dcb06dfedf9abacc5f979332ae2a30732dfed145
Cooper b. Anderson <cooper@cooperuser.dev>
2025-10-03 20:31:11 -0500

    kick: Wrap clipboard setting in vim.schedule

--- init.lua
+++ init.lua
@@ -35,9 +35,12 @@ vim.opt.relativenumber = true
 vim.opt.showmode = false
 
 -- Sync clipboard between OS and Neovim.
+--  Schedule the setting after `UiEnter` because it can increase startup-time.
 --  Remove this option if you want your OS clipboard to remain independent.
 --  See `:help 'clipboard'`
-vim.opt.clipboard = "unnamedplus"
+vim.schedule(function()
+	vim.o.clipboard = "unnamedplus"
+end)
 
 -- Enable break indent
 vim.opt.breakindent = true
