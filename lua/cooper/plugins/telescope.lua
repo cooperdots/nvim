9b748c59cf37a455a643592a8952f2d13cc1c8fb
Cooper b. Anderson <cooper@cooperuser.dev>
2025-10-03 20:55:53 -0500

    kick: Update telescope configuration

--- lua/cooper/plugins/telescope.lua
+++ lua/cooper/plugins/telescope.lua
@@ -1,7 +1,6 @@
 return { -- Fuzzy Finder (files, lsp, etc)
 	"nvim-telescope/telescope.nvim",
 	event = "VimEnter",
-	branch = "0.1.x",
 	dependencies = {
 		"nvim-lua/plenary.nvim",
 		{ -- If encountering errors, see telescope-fzf-native README for installation instructions
@@ -50,7 +49,7 @@ return { -- Fuzzy Finder (files, lsp, etc)
 			--
 			-- defaults = {
 			--   mappings = {
-			--     i = { ['<c-enter>'] = 'to_fuzzy_refine' },
+			--     i = { ["<c-enter>"] = "to_fuzzy_refine" },
 			--   },
 			-- },
 			-- pickers = {}
@@ -82,7 +81,7 @@ return { -- Fuzzy Finder (files, lsp, etc)
 		vim.keymap.set("n", "<leader>sG", builtin.live_grep, { desc = "[S]earch by [G]rep" })
 		vim.keymap.set("n", "<leader>sd", builtin.diagnostics, { desc = "[S]earch [D]iagnostics" })
 		vim.keymap.set("n", "<leader>sr", builtin.resume, { desc = "[S]earch [R]esume" })
-		vim.keymap.set("n", "<leader>s.", builtin.oldfiles, { desc = '[S]earch Recent Files ("." for repeat)' })
+		vim.keymap.set("n", "<leader>s.", builtin.oldfiles, { desc = "[S]earch Recent Files ('.' for repeat)" })
 		vim.keymap.set("n", "<leader><leader>", builtin.buffers, { desc = "[ ] Find existing buffers" })
 
 		-- Slightly advanced example of overriding default behavior and theme
