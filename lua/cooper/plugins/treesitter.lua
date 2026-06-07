3f314fc21f35108c303c6d09fc0bc5bf2e041de5
Cooper b. Anderson <cooper@cooperuser.dev>
2026-06-07 18:51:53 +0000

    Move treesitter config to plugins folder

---
+++ lua/cooper/plugins/treesitter.lua
@@ -0,0 +1,31 @@
+return {
+	{
+		"nvim-treesitter/nvim-treesitter",
+		lazy = false,
+		branch = "main",
+		build = ":TSUpdate",
+		opts = {
+			ensure_installed = {
+				"bash",
+				"diff",
+				"html",
+				"lua",
+				"luadoc",
+				"markdown",
+				"markdown_inline",
+				"query",
+				"rust",
+				"vim",
+				"vimdoc",
+			},
+			-- Autoinstall languages that are not installed
+			auto_install = true,
+			highlight = {
+				enable = true,
+			},
+			indent = {
+				enable = true,
+			},
+		},
+	},
+}
