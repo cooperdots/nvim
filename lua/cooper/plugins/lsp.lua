84133e09a2d376aa4e63ac86755b4886af49af6d
Cooper b. Anderson <cooper@cooperuser.dev>
2026-06-13 13:37:40 -0500

    Enable rust-analyzer

--- lua/cooper/plugins/lsp.lua
+++ lua/cooper/plugins/lsp.lua
@@ -229,6 +229,7 @@ return { -- LSP Configuration & Plugins
 				-- ts_ls = {},
 			}
 
+			vim.lsp.enable("rust_analyzer")
 			vim.lsp.config("rust_analyzer", {
 				settings = {
 					["rust-analyzer"] = {
